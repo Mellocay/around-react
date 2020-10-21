04274b0ebe2ec5da4c041e6a51d8698b072cc54c
unknown <mellocay@gmail.com>
2020-10-20 22:02:14 -0700

    First step complete

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header">
+      <img src={headerLogo} alt="Around the US Logo" className="header__logo" />
+    </header>
+  );
+}
+
+export default Header;
