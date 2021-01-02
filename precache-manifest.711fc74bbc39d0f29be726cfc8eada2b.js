5ea23548b6729db181263a2732e024e381cf3c41
unknown <mellocay@gmail.com>
2021-01-02 13:42:03 -0800

    Updates

---
+++ precache-manifest.711fc74bbc39d0f29be726cfc8eada2b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb98b15c9bd6559dbc9f7675184adcc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d27a45aaf9edb835a11",
+    "url": "/static/css/main.48bb486a.chunk.css"
+  },
+  {
+    "revision": "bea6491ae21ade61264d",
+    "url": "/static/js/2.ff751c37.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ff751c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d27a45aaf9edb835a11",
+    "url": "/static/js/main.fafc1565.chunk.js"
+  },
+  {
+    "revision": "076524b2c5b339f12c41",
+    "url": "/static/js/runtime-main.f1369426.js"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/add-button.b2fce4ac.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/close-x.c2d0901f.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/edit-button.6bfdb354.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/static/media/heart.83a46f45.svg"
+  },
+  {
+    "revision": "463efba3b490de89ecd0a0f1f3b5685b",
+    "url": "/static/media/heart_hover.463efba3.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/static/media/logo.8f7611ae.svg"
+  },
+  {
+    "revision": "4220563aa7e7f6bb1d562a95174678b6",
+    "url": "/static/media/trash-can.4220563a.svg"
+  }
+]);
